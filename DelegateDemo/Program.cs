0df68621705f1cba2a0e54e020b69d1537e3653c
Afreen Shaik <afreendillu@gmail.com>
2021-06-07 15:59:08 +0530

    push the folder

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+      
+            // 1.Creating a delegate
+        public delegate int MyDelegate(int num1, int num2);
+        static void Main(string[] args)
+        {
+            //3.Create Object for delegate
+            MyDelegate obj = new MyDelegate(AddNumbers); //Function Pointer // obj += new MyDelegate(SubNumbers);co
+
+
+
+            //Invoke delegate
+            int resDel = obj(55, 44);
+
+            Console.WriteLine("result is:" + resDel);
+            Console.ReadLine();
+
+            int res = AddNumbers(55, 44);
+            int res2 = SubNumbers(55, 44);
+            int res3 = MultNumbers(55, 44);
+        }
+
+
+
+
+        //2. Creating Methods that will support the Delegate parameter
+        public static int AddNumbers(int num1, int numberr2)
+        {
+            return num1 + numberr2;
+        }
+        public static int SubNumbers(int num1, int numberr2)
+        {
+            return num1 + numberr2;
+        }
+        public static int MultNumbers(int num1, int numberr2)
+        {
+            return num1 + numberr2;
+        }
+    }
+    }
+
